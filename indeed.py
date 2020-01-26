53cb0770520c3747cb72a81d845678901b26a0e7
IPSAE LEE <lareina.ipse@gmail.com>
2020-01-26 06:05:21 +0000

    indeed Title, Company, Location, Link 가져오기

--- indeed.py
+++ indeed.py
@@ -1,8 +1,9 @@
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 
-LIMIT = 10
+LIMIT = 50
 URL = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=%EC%9E%90%EB%B0%94&limit={LIMIT}"
+
 
 def extract_indeed_pages():
     result = requests.get(URL)
@@ -10,22 +11,37 @@ def extract_indeed_pages():
     pagination = soup.find("div", {"class": "pagination"})
     links = pagination.find_all('a')
     pages = []
-    for link in links[:-1] :
+    for link in links[:-1]:
         pages.append(int(link.string))
     max_page = pages[-1]
-
     return max_page
 
-    
+
+def extract_job(html):
+    title = html.find("div", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+
+    job_id = html["data-jk"]
+    print(job_id)
+    return {'title': title, 'company': company, 'location': location, "link": f"http://www.indeed.com/viewjob?jk={job_id}"}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
 
-    for result in results:
-        title = result.find("div", {"class": "title"})
-        anchor = title.find("a")["title"]
-        print(anchor)
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
