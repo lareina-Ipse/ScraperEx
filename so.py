4f52d105e02c0e4752aa2dd53c6e2baf52aa9cae
IPSAE LEE <lareina.ipse@gmail.com>
2020-01-26 17:05:24 +0000

    Done Scrapping

--- so.py
+++ so.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 
+
 def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
@@ -11,22 +12,36 @@ def get_last_page():
     last_page = pages[-1].get_text(strip=True)
     return int(last_page)
 
+
 def extract_job(html):
     title = html.find("div", {"class": "fl1"}).find("a")["title"]
-    company, location = html.find("h3", {"class": "mb4"}).find_all("span", recursive=False)
-    print(company.get_text(strip=True), location.get_text(strip=True))
-    return {'title': title}
+    company, location = html.find("h3", {
+        "class": "mb4"
+    }).find_all(
+        "span", recursive=False)
+    company = company.get_text(strip=True).strip("\n")
+    location = location.get_text(
+        strip=True).strip("-").strip(" \r").strip("\n")
+    job_id = html['data-jobid']
+    return {
+        'title': title,
+        'company': company,
+        'link': f"https://stackoverflow.com/jobs/{job_id}"
+    }
+
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        result = requests.get(f"{URL}&pg={page}")
+        print(f"Scrapping SO: Page: {page}")
+        result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
             job = extract_job(result)
             jobs.append(job)
     return jobs
+
 
 def get_jobs():
     last_page = get_last_page()
